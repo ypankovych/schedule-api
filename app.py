03bb2ea655d8d9ab8c3cafb9992d99bf1037a830
Pavel Durmanov <alban@national.shitposting.agency>
2019-01-06 10:06:33 +0200

    change docs title

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-import os
-
 from flasgger import Swagger
 from flask import Flask, redirect
 from flask_restful import Api
 from webargs.flaskparser import parser, abort
+import os
 
-from common.config import template
-from resources.lists import AllGroups, AllTeachers
 from resources.schedule import Group, Teacher
+from resources.lists import AllGroups, AllTeachers
+from common.config import template
 
 app = Flask(__name__)
 app.config['RESTFUL_JSON'] = {
     'ensure_ascii': False
+}
+app.config['SWAGGER'] = {
+    'title': 'Schedule API',
+    'uiversion': 3,
 }
 
 api = Api(app)
